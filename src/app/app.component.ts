7dd98fb80cd83cbb5681fd3e7aad4de8e52f2cb1
Георгий <drug200221@gmail.com>
2025-04-10 14:26:34 +0300

    Подключен angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'psk-root',
+  imports: [MatToolbar, MatIcon, MatIconButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
