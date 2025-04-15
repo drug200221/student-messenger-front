76ecd99a9bf4fafb4546688fadfb9c9e30d9d817
Георгий <drug200221@gmail.com>
2025-04-15 17:03:49 +0300

    0.x/sidenav Сделал message-area - доверстать

--- src/app/user/components/chats/chats.component.ts
+++ src/app/user/components/chats/chats.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ChatItemComponent } from '../chat-item/chat-item.component';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
 import { MatFormField, MatInput, MatSuffix } from '@angular/material/input';
@@ -7,6 +7,7 @@ import { MatListItem, MatSelectionList } from '@angular/material/list';
 import { MatFabButton, MatIconButton } from '@angular/material/button';
 import { MatTooltip } from '@angular/material/tooltip';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'psk-chats',
@@ -31,7 +32,14 @@ import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
   templateUrl: './chats.component.html',
   styleUrl: './chats.component.scss',
 })
-export class ChatsComponent {
-  public items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+export class ChatsComponent implements OnInit {
 
+  public items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  private route = inject(ActivatedRoute);
+  public chatId: number = +this.route.snapshot.params['chatId'];
+
+  public ngOnInit(): void {
+    this.route.params.subscribe(params => console.log(params));
+    console.log(this.chatId);
+  }
 }
--- src/app/user/components/message-area/message-area.component.ts
+++ src/app/user/components/message-area/message-area.component.ts
@@ -4,6 +4,8 @@ import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 import { MatTooltip } from '@angular/material/tooltip';
+import { MatFormField, MatInput, MatSuffix } from '@angular/material/input';
+import { MatCard, MatCardContent, MatCardFooter } from '@angular/material/card';
 
 @Component({
   selector: 'psk-message-area',
@@ -15,6 +17,12 @@ import { MatTooltip } from '@angular/material/tooltip';
     MatMenu,
     MatMenuItem,
     MatTooltip,
+    MatFormField,
+    MatInput,
+    MatSuffix,
+    MatCard,
+    MatCardContent,
+    MatCardFooter,
   ],
   templateUrl: './message-area.component.html',
   styleUrl: './message-area.component.scss',
